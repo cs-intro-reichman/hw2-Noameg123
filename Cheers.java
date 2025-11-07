2c57c05d2fbd0b7e6a9bdffba5fcc5f04a07f51e
Noa Meghnagi <noameghnagi@Noas-MacBook-Air.local>
2025-11-07 15:20:19 +0200

    second try

--- Cheers.java
+++ Cheers.java
@@ -4,23 +4,22 @@ Great work boaz!
 */
 // Prints a crowd cheering output.
 public class Cheers {
-        public static void main(String[] args) {
-        String name = args[0];
-        String an_letter = "{'A', 'E', 'F', 'H', 'I', 'L', 'M', 'N', 'O', 'R', 'S', 'X'}";
-        int times = Integer.parseInt(args[1]); 
-        for(int i = 0; i < name.length(); i++) {
-                char letter = name.charAt(i);
-                if (an_letter.indexOf(letter)!= -1){
-                        System.out.println("Give me an " + letter + ": "+ letter + "!");
-                }
-                else{
-                  System.out.println("Give me a " + letter + ": "+ letter + "!");
-                }
-        }
-        
-        for(int i = 0; i < times; i++) {
-                        System.out.println(name+ "!!!");
-        }
+    public static void main(String[] args) {
+        String name = args[0].toUpperCase(); // הופך את השם לאותיות גדולות
+        String an_letters = "AEFHILMNORSX";  
+        int times = Integer.parseInt(args[1]);
+
+        for (int i = 0; i < name.length(); i++) {
+            char letter = name.charAt(i);
+            if (an_letters.indexOf(letter) != -1) {
+                System.out.println("Give me an " + letter + ": " + letter + "!");
+            } else {
+                System.out.println("Give me a " + letter + ": " + letter + "!");
+            }
         }
 
+        for (int i = 0; i < times; i++) {
+            System.out.println(name + "!!!");
+        }
+    }
 }
