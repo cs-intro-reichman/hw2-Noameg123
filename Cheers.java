7fc12338484fe5d838b4b262129ccd64c752b526
Noa Meghnagi <noameghnagi@Noas-MacBook-Air.local>
2025-11-07 15:15:42 +0200

    first try

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,26 @@
 //feedback
-
 /*
 Great work boaz!
 */
-
-
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+        String name = args[0];
+        String an_letter = "{'A', 'E', 'F', 'H', 'I', 'L', 'M', 'N', 'O', 'R', 'S', 'X'}";
+        int times = Integer.parseInt(args[1]); 
+        for(int i = 0; i < name.length(); i++) {
+                char letter = name.charAt(i);
+                if (an_letter.indexOf(letter)!= -1){
+                        System.out.println("Give me an " + letter + ": "+ letter + "!");
+                }
+                else{
+                  System.out.println("Give me a " + letter + ": "+ letter + "!");
+                }
         }
+        
+        for(int i = 0; i < times; i++) {
+                        System.out.println(name+ "!!!");
+        }
+        }
+
 }
