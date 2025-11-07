811c27c378da53e12ba47f50d9699dac1cc59d62
Noa Meghnagi <noameghnagi@Noas-MacBook-Air.local>
2025-11-07 15:26:51 +0200

    4 try

--- Cheers.java
+++ Cheers.java
@@ -14,7 +14,7 @@ public class Cheers {
             if (an_letters.indexOf(letter) != -1) {
                 System.out.println("Give me an " + letter + ": " + letter + "!");
             } else {
-                System.out.println("Give me a " + letter + ": " + letter + "!");
+                System.out.println("Give me a  " + letter + ": " + letter + "!");
             }
         }
         System.out.println("What does that spell?");
